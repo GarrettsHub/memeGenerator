66ec31d76450777a548d30eef6a48597515b3ce0
GarrettsHub <helixsparrk@gmail.com>
2024-05-13 15:18:35 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header =()=> {
+    return (
+        <header className="header" id="header">
+            <div className="container">
+                <div className="col-md-4">
+                    <h1 className="heading text-capitalize">meme generator</h1>
+                </div>
+                <div className="col-md-4">
+                    <h2 className="sub-heading text-capitalize">react course - project 3</h2>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
